07a59cc75f823d718996d1c3ee496b08c8229a89
Daniel MF <echo.dmf@gmail.com>
2018-09-17 14:19:14 -0300

    extra tests

--- ios/Classes/OpenFilePlugin.m
+++ ios/Classes/OpenFilePlugin.m
@@ -25,6 +25,7 @@ static NSString *const CHANNEL_NAME = @"open_file";
     self = [super init];
     if (self) {
         _viewController = viewController;
+        _documentController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:msg]];
     }
     return self;
 }
@@ -37,61 +38,62 @@ static NSString *const CHANNEL_NAME = @"open_file";
         if(fileExist){
 //            NSURL *resourceToOpen = [NSURL fileURLWithPath:msg];
             NSString *exestr = [msg pathExtension];
+            _documentController.delegate = self;
             if([exestr isEqualToString:@"rtf"]){
-                _viewController.UTI=@"public.rtf";
+                _documentController.UTI=@"public.rtf";
             }else if([exestr isEqualToString:@"txt"]){
-                _viewController.UTI=@"public.plain-text";
+                _documentController.UTI=@"public.plain-text";
             }else if([exestr isEqualToString:@"html"]||
                      [exestr isEqualToString:@"htm"]){
-                _viewController.UTI=@"public.html";
+                _documentController.UTI=@"public.html";
             }else if([exestr isEqualToString:@"xml"]){
-                _viewController.UTI=@"public.xml";
+                _documentController.UTI=@"public.xml";
             }else if([exestr isEqualToString:@"tar"]){
-                _viewController.UTI=@"public.tar-archive";
+                _documentController.UTI=@"public.tar-archive";
             }else if([exestr isEqualToString:@"gz"]||
                      [exestr isEqualToString:@"gzip"]){
-                _viewController.UTI=@"org.gnu.gnu-zip-archive";
+                _documentController.UTI=@"org.gnu.gnu-zip-archive";
             }else if([exestr isEqualToString:@"tgz"]){
-                _viewController.UTI=@"org.gnu.gnu-zip-tar-archive";
+                _documentController.UTI=@"org.gnu.gnu-zip-tar-archive";
             }else if([exestr isEqualToString:@"jpg"]||
                      [exestr isEqualToString:@"jpeg"]){
-                _viewController.UTI=@"public.jpeg";
+                _documentController.UTI=@"public.jpeg";
             }else if([exestr isEqualToString:@"png"]){
-                _viewController.UTI=@"public.png";
+                _documentController.UTI=@"public.png";
             }else if([exestr isEqualToString:@"avi"]){
-                _viewController.UTI=@"public.avi";
+                _documentController.UTI=@"public.avi";
             }else if([exestr isEqualToString:@"mpg"]||
                      [exestr isEqualToString:@"mpeg"]){
-                _viewController.UTI=@"public.mpeg";
+                _documentController.UTI=@"public.mpeg";
             }else if([exestr isEqualToString:@"mp4"]){
-                _viewController.UTI=@"public.mpeg-4";
+                _documentController.UTI=@"public.mpeg-4";
             }else if([exestr isEqualToString:@"3gpp"]||
                      [exestr isEqualToString:@"3gp"]){
-                _viewController.UTI=@"public.3gpp";
+                _documentController.UTI=@"public.3gpp";
             }else if([exestr isEqualToString:@"mp3"]){
-                _viewController.UTI=@"public.mp3";
+                _documentController.UTI=@"public.mp3";
             }else if([exestr isEqualToString:@"zip"]){
-                _viewController.UTI=@"com.pkware.zip-archive";
+                _documentController.UTI=@"com.pkware.zip-archive";
             }else if([exestr isEqualToString:@"gif"]){
-                _viewController.UTI=@"com.compuserve.gif";
+                _documentController.UTI=@"com.compuserve.gif";
             }else if([exestr isEqualToString:@"bmp"]){
-                _viewController.UTI=@"com.microsoft.bmp";
+                _documentController.UTI=@"com.microsoft.bmp";
             }else if([exestr isEqualToString:@"ico"]){
-                _viewController.UTI=@"com.microsoft.ico";
+                _documentController.UTI=@"com.microsoft.ico";
             }else if([exestr isEqualToString:@"doc"]){
-                _viewController.UTI=@"com.microsoft.word.doc";
+                _documentController.UTI=@"com.microsoft.word.doc";
             }else if([exestr isEqualToString:@"xls"]){
-                _viewController.UTI=@"com.microsoft.excel.xls";
+                _documentController.UTI=@"com.microsoft.excel.xls";
             }else if([exestr isEqualToString:@"ppt"]){
-                _viewController.UTI=@"com.microsoft.powerpoint.​ppt";
+                _documentController.UTI=@"com.microsoft.powerpoint.​ppt";
             }else if([exestr isEqualToString:@"wav"]){
-                _viewController.UTI=@"com.microsoft.waveform-​audio";
+                _documentController.UTI=@"com.microsoft.waveform-​audio";
             }else if([exestr isEqualToString:@"wm"]){
-                _viewController.UTI=@"com.microsoft.windows-​media-wm";
+                _documentController.UTI=@"com.microsoft.windows-​media-wm";
             }else if([exestr isEqualToString:@"wmv"]){
-                _viewController.UTI=@"com.microsoft.windows-​media-wmv";
+                _documentController.UTI=@"com.microsoft.windows-​media-wmv";
             }else if([exestr isEqualToString:@"pdf"]){
-                _viewController.UTI=@"com.adobe.pdf";
+                _documentController.UTI=@"com.adobe.pdf";
             }else {
                 NSLog(@"doc type not supported for preview");
                 NSLog(exestr);
@@ -99,7 +101,7 @@ static NSString *const CHANNEL_NAME = @"open_file";
             
             NSLog(@"openning file");
             
-            [_viewController presentOpenInMenuFromRect:CGRectMake(500,20,100,100) inView:[UIApplication sharedApplication].delegate.window.rootViewController.view animated:YES];
+            [_documentController presentOpenInMenuFromRect:CGRectMake(500,20,100,100) inView:[UIApplication sharedApplication].delegate.window.rootViewController.view animated:YES];
             
             result(@"done");
             
