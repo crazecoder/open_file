99c87f572d1c6cea8e565d1c2f316be70a79eac0
crazecoder <21527312@qq.com>
2019-09-20 14:38:30 +0800

    add pc support(beta)

---
+++ lib/src/open_file.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'macos.dart' as mac;
+import 'linux.dart' as linux;
+import 'windows.dart' as windows;
+
+class OpenFile {
+  static const MethodChannel _channel = const MethodChannel('open_file');
+
+  ///linuxDesktopName like 'xdg'/'gnome'
+  static Future<String> open(String filePath,
+      {String type, String uti, String linuxDesktopName}) async {
+    if (!Platform.isIOS && !Platform.isAndroid) {
+      int _result;
+      //file or url
+      if (Platform.isMacOS) {
+        _result = mac.system('open $filePath');
+      } else if (Platform.isLinux) {
+        _result = linux.system(
+            '${linuxDesktopName.isEmpty ? "" : "${linuxDesktopName.isEmpty}-"}open $filePath');
+      }else{
+        _result = windows.ShellExecute('open',filePath);
+      }
+      return _result == 0
+          ? "done"
+          : "there are some errors when open $filePath";
+    }
+
+    Map<String, String> map = {"file_path": filePath, "type": type, "uti": uti};
+    return await _channel.invokeMethod('open_file', map);
+  }
+}
