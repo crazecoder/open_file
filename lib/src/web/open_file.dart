c578681e3a4ad462f0acd70e4e75cca2375368fe
crazecoder <21527312@qq.com>
2020-01-18 09:51:07 +0800

    fix #80

--- lib/src/web/open_file.dart
+++ lib/src/web/open_file.dart
@@ -4,12 +4,11 @@ import 'package:open_file/src/common/open_result.dart';
 import 'web.dart' as web;
 
 class OpenFile {
-  static Future<OpenResult> open(String filePath) async {
+  static Future<OpenResult> open(String filePath,
+      {String type, String uti, String linuxDesktopName = "xdg"}) async {
     bool _b = await web.open("file://$filePath");
     return OpenResult(
         type: _b ? ResultType.done : ResultType.error,
-        message: _b
-            ? "done"
-            : "there are some errors when open $filePath");
+        message: _b ? "done" : "there are some errors when open $filePath");
   }
 }
