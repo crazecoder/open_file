a3f46757362cf7beff3e498360c27515fe863792
crazecoder <21527312@qq.com>
2021-03-09 17:14:29 +0800

    add linuxByProcess parameter

--- lib/src/plaform/open_file.dart
+++ lib/src/plaform/open_file.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:open_file/src/common/open_result.dart';
 import 'macos.dart' as mac;
 import 'windows.dart' as windows;
+import 'linux.dart' as linux;
 
 class OpenFile {
   static const MethodChannel _channel = const MethodChannel('open_file');
@@ -14,13 +15,17 @@ class OpenFile {
 
   ///linuxDesktopName like 'xdg'/'gnome'
   static Future<OpenResult> open(String filePath,
-      {String? type, String? uti, String linuxDesktopName = "xdg"}) async {
+      {String? type, String? uti, String linuxDesktopName = "xdg", bool linuxByProcess = false}) async {
     if (!Platform.isIOS && !Platform.isAndroid) {
       int _result;
       if (Platform.isMacOS) {
         _result = mac.system('open $filePath');
       } else if (Platform.isLinux) {
-        _result = Process.runSync('xdg-open', [filePath]).exitCode;
+        if (linuxByProcess) {
+          _result = Process.runSync('xdg-open', [filePath]).exitCode;
+        } else {
+          _result = linux.system('$linuxDesktopName-open "$filePath"');
+        }
       } else {
         _result = windows.shellExecute('open', filePath);
       }
