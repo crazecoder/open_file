3f25d66841c7426d6fcd571ee46ea4b784fcaaf1
crazecoder <21527312@qq.com>
2021-05-28 14:10:06 +0800

    Synchronous Web Methods

--- lib/src/web/open_file.dart
+++ lib/src/web/open_file.dart
@@ -4,8 +4,12 @@ import 'package:open_file/src/common/open_result.dart';
 import 'web.dart' as web;
 
 class OpenFile {
-  static Future<OpenResult> open(String filePath,
-      {String? type, String? uti, String linuxDesktopName = "xdg"}) async {
+  OpenFile._();
+  static Future<OpenResult> open(String? filePath,
+      {String? type,
+        String? uti,
+        String linuxDesktopName = "xdg",
+        bool linuxByProcess = false}) async {
     final _b = await web.open("file://$filePath");
     return OpenResult(
         type: _b ? ResultType.done : ResultType.error,
