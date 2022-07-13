ef0aa9ee6dbddbd87492cbae70ba47196e09f5aa
40in <62744385+SergeySor@users.noreply.github.com>
2022-07-13 10:55:13 +0300

    Update parse_args.dart

--- lib/src/common/parse_args.dart
+++ lib/src/common/parse_args.dart
@@ -1,9 +1,11 @@
 String parseArgs(List<String> args) {
     final commandList = args
-        .map((arg) => arg
-            .replaceAll(' ', '\\ ')
-            .replaceAll('(', '\\(')
-            .replaceAll(')', '\\)'))
+        .map(
+          (arg) => arg
+              .replaceAll(' ', '\\ ')
+              .replaceAll('(', '\\(')
+              .replaceAll(')', '\\)'),
+        )
         .toList();
 
     return commandList.join(' ');
