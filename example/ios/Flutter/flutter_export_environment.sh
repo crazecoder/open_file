d3231c7969c04d11865629d70628bb790fff2535
Bertoldo Fonseca <bertoldo@gmail.com>
2022-09-17 09:54:29 -0300

    refactor: remove READ_EXTERNAL_STORAGE and update some configurations

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/bertoldo/Devel/tools/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/bertoldo/Devel/git-3rdparty/flutter-better_open_file/example"
+export "FLUTTER_ROOT=/opt/devtools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/bertoldo/Devel/better_open_file/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/bertoldo/Devel/git-3rdparty/flutter-better_open_file/example/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/bertoldo/Devel/git-3rdparty/flutter-better_open_file/example/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
