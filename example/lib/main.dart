0ee6d69b5810e066ceb23776cea3716749038669
crazecoder <21527312@qq.com>
2023-04-27 11:26:46 +0800

    example for use 3.3.1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,9 +15,56 @@ class _MyAppState extends State<MyApp> {
   var _openResult = 'Unknown';
 
   Future<void> openFile() async {
+    _openAppPrivateFile();
+  }
+
+  _openAppPrivateFile() async {
+    //open an app private storage file
+    final result = await OpenFile.open(
+        "/sdcard/Android/data/com.crazecoder.openfileexample/R-C.jpeg");
+    setState(() {
+      _openResult = "type=${result.type}  message=${result.message}";
+    });
+  }
+
+  // ignore: unused_element
+  _openExternalImage() async {
+    //open an external storage image file on android 13
+    if (await Permission.photos.request().isGranted) {
+      final result = await OpenFile.open("/sdcard/Download/R-C.jpeg");
+      setState(() {
+        _openResult = "type=${result.type}  message=${result.message}";
+      });
+    }
+  }
+
+  // ignore: unused_element
+  _openExternalVideo() async {
+    //open an external storage video file on android 13
+    if (await Permission.videos.request().isGranted) {
+      final result = await OpenFile.open("/sdcard/Download/R-C.mp4");
+      setState(() {
+        _openResult = "type=${result.type}  message=${result.message}";
+      });
+    }
+  }
+
+  // ignore: unused_element
+  _openExternalAudio() async {
+    //open an external storage audio file on android 13
+    if (await Permission.audio.request().isGranted) {
+      final result = await OpenFile.open("/sdcard/Download/R-C.mp3");
+      setState(() {
+        _openResult = "type=${result.type}  message=${result.message}";
+      });
+    }
+  }
+
+  // ignore: unused_element
+  _openExternalFile() async {
+    //open an external storage file
     if (await Permission.manageExternalStorage.request().isGranted) {
-      final result =
-          await OpenFile.open("/sdcard/Download/test.jpg");
+      final result = await OpenFile.open("/sdcard/Android/data/R-C.xml");
       setState(() {
         _openResult = "type=${result.type}  message=${result.message}";
       });
