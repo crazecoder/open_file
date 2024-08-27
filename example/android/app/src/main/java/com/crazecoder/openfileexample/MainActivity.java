6a6c7f984caa93843d4b5005ec527d6491135836
crazecoder <21527312@qq.com>
2024-08-28 00:07:13 +0800

    remove

--- example/android/app/src/main/java/com/crazecoder/openfileexample/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.crazecoder.openfileexample;
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/open_file.dart
+++
@@ -1,5 +0,0 @@
-library open_file;
-
-export 'src/common/open_result.dart';
-export 'src/platform/open_file.dart'
-    if (dart.library.html) 'src/web/open_file.dart';
